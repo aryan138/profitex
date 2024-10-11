4f4a50990c8d2de9b8a6e1bb4b930338738d947c
aryan 138 <apathania206@gmail.com>
2024-10-12 03:29:03 +0530

    order jwt rm

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const {verifyJwt} = require('../middlewares/authorize');
+const { verifyJwt } = require('../middlewares/authorize');
 
-router.post('/placeorder',verifyJwt, orderController.placeorder);
-router.get('/getorder',verifyJwt, orderController.getOrder);
+router.post('/placeorder', orderController.placeorder);
+router.get('/getorder', orderController.getOrder);
 router.get('/getorderbyid/:id', orderController.getOrderById);
 router.put('/updateorder/:id', orderController.updateorder);
 router.delete('/deleteorder/:id', orderController.deleteorder);
