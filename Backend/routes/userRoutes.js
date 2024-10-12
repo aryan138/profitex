4a7f75de294104f5222c41b8a5ca48157ee88aed
aryan 138 <apathania206@gmail.com>
2024-10-12 16:06:04 +0530

    get details jwt rm

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usercontroller');
-const {verifyJwt} = require('../middlewares/authorize');
+const { verifyJwt } = require('../middlewares/authorize');
 
-router.post("/register",userController.register);
-router.post("/sign-in",userController.loginUser);
-router.get("/logout",verifyJwt,userController.logoutUser);
-router.post("/update-details",userController.updateUserDetails);
-router.get("/get-details",verifyJwt,userController.getDetails);
+router.post("/register", userController.register);
+router.post("/sign-in", userController.loginUser);
+router.get("/logout", userController.logoutUser);
+router.post("/update-details", userController.updateUserDetails);
+router.get("/get-details", userController.getDetails);
 module.exports = router;
